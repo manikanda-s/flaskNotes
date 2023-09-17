029ee97e9d79b31ec87beedb95b72b33fcdce8ee
manikandan <manikandan-selvam@outlook.com>
2023-09-17 13:15:42 +0530

    pass dynamic argument through url

--- app-03.py
+++ app-03.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/home')
-def home():
-    return 'Hello world'
+@app.route('/home/<name>')
+def home(name):
+    return f'Hello world {name}'
 
 if (__name__ == '__main__'):
     app.run(debug=True)
